7085a1013041dbad75fe3cfb57d134425568154e
Chris Closser <chrisclosser1@gmail.com>
2017-12-12 09:56:44 -0800

    have some soup stackoverflow

--- src/scrypt.py
+++ src/scrypt.py
@@ -7,17 +7,16 @@ import requests
 
 def stack_jobs():
     """Use requests and BS to find public twitter profile and harvest information from HTML."""
-    try:
-        url = 'https://stackoverflow.com/jobs'
-        r = requests.get(url)
-        soup = BeautifulSoup(r.content, 'lxml')
-        import pdb; pdb.set_trace()
 
-    except:
-        return {'site': 'stackoverflow',
-                'empty': 'Not found'
-                }
-
+    job_list = []
+    url = 'https://stackoverflow.com/jobs'
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, 'lxml')
+    jobs = soup.find_all(class_='-title g-row')
+    for idx in jobs:
+        # import pdb; pdb.set_trace()
+        job_list.append(jobs[idx].text)
+    return job_list
 
 if __name__ == '__main__':
-    stack_jobs()
+    print(stack_jobs())
